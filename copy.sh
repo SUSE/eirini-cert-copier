f041c265d92fcce599abb66db7d1e583324e89f7
viovanov <vlad.iovanov@suse.com>
2019-03-14 15:43:35 +0200

    Simplify cert copying

--- copy.sh
+++ copy.sh
@@ -2,21 +2,8 @@
 
 readonly REGISTRY_CERTS_DIR="/workspace/docker/certs.d/$REGISTRY/"
 
-copy-cert() {
-    mkdir --parents "$REGISTRY_CERTS_DIR"
-    echo "${INTERNAL_CA_CERT}" | base64 -d > "$REGISTRY_CERTS_DIR/ca.crt"
-    echo "Sucessfully copied certs"
-}
-
-main(){
-  while true; do
-    if get-cert; then
-        copy-cert
-    else
-        echo "Nothing to do"
-    fi
-    sleep 30
-  done
-}
+mkdir --parents "$REGISTRY_CERTS_DIR"
+echo "${INTERNAL_CA_CERT}" | base64 -d > "$REGISTRY_CERTS_DIR/ca.crt"
+echo "Sucessfully copied certs"
 
 main
