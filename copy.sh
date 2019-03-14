d8e7208121a02e98b9c0a5ec1efc4191bd08449c
viovanov <vlad.iovanov@suse.com>
2019-03-14 15:50:03 +0200

    Fix cert copy script

--- copy.sh
+++ copy.sh
@@ -3,7 +3,5 @@
 readonly REGISTRY_CERTS_DIR="/workspace/docker/certs.d/$REGISTRY/"
 
 mkdir --parents "$REGISTRY_CERTS_DIR"
-echo "${INTERNAL_CA_CERT}" | base64 -d > "$REGISTRY_CERTS_DIR/ca.crt"
+echo "${INTERNAL_CA_CERT}" > "$REGISTRY_CERTS_DIR/ca.crt"
 echo "Sucessfully copied certs"
-
-main
